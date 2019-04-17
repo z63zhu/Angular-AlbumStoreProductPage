80af42161de3b7d4c6474697715d4ff977432f4f
z63zhu <z63zhu@outlook.com>
2019-04-17 17:46:56 -0400

    t34

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from "@angular/core";
-import { ProductService } from "../product.service";
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
-  selector: "app-product-description",
-  templateUrl: "./product-description.component.html",
-  styleUrls: ["./product-description.component.css"]
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
   albumInfo;
   constructor(private _productService: ProductService) {}
 
   ngOnInit() {
-    this._productService
-      .getAlbum(1)
-      .subscribe(response => (this.albumInfo = response));
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 }
