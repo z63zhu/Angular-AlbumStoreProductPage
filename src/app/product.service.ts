2aa0fd6698ee7231c8fa310fde5a2ebe0a60d904
z63zhu <z63zhu@outlook.com>
2019-04-18 10:04:46 -0400

    t41

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
-import "rxjs/add/operator/map";
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Album} from './album';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
-  private _albumUrl = "../assets/album.json";
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) {}
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 }
